ae8eeab265ad844bb4668e430de068ffaea25b09
Menna Rasslan <132094665+MennaT-T@users.noreply.github.com>
2025-03-15 17:55:00 +0200

    seam carving with forward energy

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
 from seam_carving import SeamCarver
-
 import os
 
-
-
-def image_resize_without_mask(filename_input, filename_output, new_height, new_width, visualize_seams_path):
+def image_resize(filename_input, filename_output, new_height, new_width, visualize_seams_path):
     obj = SeamCarver(filename_input, new_height, new_width)
     obj.save_result(filename_output)
     obj.save_seams_visualization(visualize_seams_path)
 
-
-def image_resize_with_mask(filename_input, filename_output, new_height, new_width, filename_mask):
-    obj = SeamCarver(filename_input, new_height, new_width, protect_mask=filename_mask)
-    obj.save_result(filename_output)
-
-
-def object_removal(filename_input, filename_output, filename_mask):
-    obj = SeamCarver(filename_input, 0, 0, object_mask=filename_mask)
-    obj.save_result(filename_output)
-
-
-
 if __name__ == '__main__':
     """
-    Put image in in/images folder and protect or object mask in in/masks folder
-    Ouput image will be saved to out/images folder with filename_output
+    Put the input image in the 'in/images' folder.
+    The output resized image and seams visualization will be saved in 'out/images'.
     """
 
     folder_in = 'in'
@@ -32,25 +17,12 @@ if __name__ == '__main__':
 
     filename_input = 'test6.jpg' 
     filename_output = 'image_result.png'
-    filename_mask = 'mask.jpg'
     visualize_seams = 'seams_visualization.png'
     new_height = 250
     new_width = 400
 
     input_image = os.path.join(folder_in, "images", filename_input)
-    input_mask = os.path.join(folder_in, "masks", filename_mask)
     output_image = os.path.join(folder_out, "images", filename_output)
     output_seams = os.path.join(folder_out, "images", visualize_seams)
-    
 
-    image_resize_without_mask(input_image, output_image, new_height, new_width, output_seams)
-    #image_resize_with_mask(input_image, output_image, new_height, new_width, input_mask)
-    #object_removal(input_image, output_image, input_mask)
-
-
-
-
-
-
-
-
+    image_resize(input_image, output_image, new_height, new_width, output_seams)
